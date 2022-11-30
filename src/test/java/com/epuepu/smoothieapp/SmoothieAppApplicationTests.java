b677379fb14b4310c2b891af1853fa8438d20a00
EpuEpu <sh4cun@gmail.com>
2022-11-30 18:21:03 +0100

    Generated project

---
+++ src/test/java/com/epuepu/smoothieapp/SmoothieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epuepu.smoothieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmoothieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
